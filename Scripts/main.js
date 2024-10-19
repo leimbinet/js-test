4a549923aafe13e3a1f3601fb2eeca524b7619c7
Ruben Barbaro <31662785+leimbinet@users.noreply.github.com>
2024-10-20 01:02:15 +0200

    wip 2 (working)

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,11 +1,3 @@
-var Vontobel = Vontobel || {};  // Initialize the main namespace if it doesn't exist
-
-Vontobel.Utilities = {
-    showMessage: function(message) {
-        console.log(message);
-    }
-};
-
 $(document).ready(function () {
     // Load default view (Home) on page load
     loadPartialView('home');
@@ -16,7 +8,9 @@ $(document).ready(function () {
         e.preventDefault(); // Prevent the default link action
 
         const view = $(this).data('view');
-        loadPartialView(view); // Load the corresponding partial view
+        if (view) {
+            loadPartialView(view); // Load the corresponding partial view
+        }
 
         setActiveMenu($(this)); // Highlight the active menu item
     });
@@ -24,7 +18,6 @@ $(document).ready(function () {
     // Function to load partial view
     function loadPartialView(view) {
         $('#partialViewContainer').load(`views/${view}.html`);
-        console.log(view);
     }
 
     // Function to highlight the active menu
